12b6422853b1e50d7b506c634bfce4f7c1ed3403
jasarria34 <jorgesarriamartinez@gmail.com>
2025-08-04 23:33:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const clearBtn = document.getElementById("clear");
+const preview = document.getElementById("firmaPreview");
 
 // Ajustar tamaño dinámico
 function resizeCanvas() {
